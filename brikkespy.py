2e11b615f5a7fc5cc331e41923671764a7589c79
storatle <atle@storler.no>
2019-10-31 10:22:58 +0100

    Lagt til mulighet for å velge poengberegning på karusellløp. Den må legges inn i heading på lister

--- brikkespy.py
+++ brikkespy.py
@@ -8,9 +8,10 @@ def main():
     parser = argparse.ArgumentParser(description='Speakermodul for Brikkesys')
     parser.add_argument('server', nargs='?', default='local', help='Server med brikkesys, local, Klara, Milo. ipadresse, brukernavn og passord må være satt i config_brikkesys.py. default=local')
     parser.add_argument('-os', '--opsys', type=str, choices=['linux','windows'], default='linux',help='Setter hvilket opesativssystem som skal benyttes. default=linux')
-    parser.add_argument('-pre', '--prewarn', type=str, help='Sett navn til forvarseldatabasen. Ipadresse, brukernavn og passord må vare satt i config_brikkesys.py')
-    parser.add_argument('-p', '--poengo', action='store_true', help='PoengO, poeng og postkoder må settes i config_poengo.py')
     parser.add_argument('-f', '--finish', action='store_true', help='Lager liste med de som kommer rett i mål')
+    parser.add_argument('-p', '--points', action='store_true', help='Legger til poengberegning for karusellløp')
+    parser.add_argument('-pre', '--prewarn', type=str, help='Sett navn til forvarseldatabasen. Ipadresse, brukernavn og passord må vare satt i config_brikkesys.py')
+    parser.add_argument('-po', '--poengo', action='store_true', help='PoengO, poeng og postkoder må settes i config_poengo.py')
     args = parser.parse_args()
 
     my_app = gui.Window()
