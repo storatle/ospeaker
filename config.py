cdbbd849abe0475e8dd8020d9c041bb800a9cf50
Atle Pedersen <atle.pedersen@sintef.no>
2022-06-16 10:51:13 +0200

    etter jeg slettet cache på SINTEFPC8879

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+def get_config(db):
+    ''"Get the appropriate parameters for connecting to the result database"""
+    
+    return {
+            
+        # local database
+        'host':'84.214.174.249',
+        'user':'root',
+        'passwd':'Purcell18',
+        'db':'resultatdatabase',
+        'charset':'utf8'
+        }       
+
